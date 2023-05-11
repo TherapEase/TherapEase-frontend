0e16fc7a8541c2550dd6c8c8a88082e2190a7a44
adiiulio <anna.diiulio2001@gmail.com>
2023-05-11 22:02:07 +0200

    Fresh reinstall without ts

--- login/src/main.ts
+++
@@ -1,9 +0,0 @@
-import { createApp } from 'vue';
-import App from './App.vue';
-import Router from "./Router.ts";
-
-// createApp(App).mount('#app')
-const app = createApp({});
-app.use(Router);
-
-
