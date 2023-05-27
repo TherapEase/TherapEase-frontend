626d3acd28754568b0dc06c17a0337dbb90aa077
Era <erestina.vreto@studenti.unitn.it>
2023-05-27 11:12:39 +0200

    creazione slot sedute

--- therapease-frontend/src/views/DashboardView.vue
+++ therapease-frontend/src/views/DashboardView.vue
@@ -2,6 +2,7 @@
   <NavBarVue />
 
   <h1><strong>Benvenuto</strong></h1>
+  <router-link to="/nuovaseduta"><button v-if="user.ruolo==2">Inserisci seduta</button></router-link>
 </template>
 
 <script>
@@ -10,9 +11,29 @@ import NavBarVue from "@/components/NavBar.vue";
 
 export default defineComponent({
   components: { NavBarVue },
-  setup() {
+  data() {
+    return {
+      user:{}
+    }
 
   },
+  async mounted(){
+    const token = sessionStorage.getItem("token");
+      const options = {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          "x-access-token": token,
+        },
+      };
+      const response = await fetch(
+        "http://localhost:3001/api/v1/il_mio_profilo",
+        options
+      );
+      const dati = await response.json();
+      console.log(JSON.stringify(dati));
+      this.user = dati["profile"];
+  }
 })
 </script>
 
--- therapease-frontend/src/views/ProfileView.vue
+++ therapease-frontend/src/views/ProfileView.vue
@@ -184,9 +184,6 @@ export default defineComponent({
         opzioniRichiesta
       );
 
-      if (!response.ok) {
-        throw new Error("Unable to get user");
-      }
       const informazioni = await response.json();
       console.log(informazioni);
       console.log(
