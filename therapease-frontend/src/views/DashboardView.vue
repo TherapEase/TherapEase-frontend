068164a6e632338e52c20beec501b4a35125ebc2
adiiulio <anna.diiulio2001@gmail.com>
2023-05-30 22:16:44 +0200

    pagina admin da testare

--- therapease-frontend/src/views/DashboardView.vue
+++ therapease-frontend/src/views/DashboardView.vue
@@ -1,6 +1,6 @@
 <template>
   <NavBarVue />
-
+<div v-if="user.ruolo == 2 || user.ruolo == 1">
   <h1>
     <strong>Ciao, {{ user.nome }}</strong>
   </h1>
@@ -28,6 +28,48 @@
   <div class="sedute">
     <SedutePrenotate :ruolo="user.ruolo" class="prenotate"></SedutePrenotate>
   </div>
+</div>
+
+<div v-if="user.ruolo == 4">
+  
+  <div class="terapeuti">
+
+    <li v-for="terapeuta in terapeuti" :key="terapeuta._id">
+                    
+    <div class="riga"><img src="../assets/profilePic.webp" alt="foto profilo" width="100">
+      <div class="colonna"><h2>{{ terapeuta.nome }} {{ terapeuta.cognome }}</h2>
+      <button class="rimozione_forzata">Rimuovi profilo</button></div>
+        
+      </div></li>
+
+  </div>
+
+  <div class="clienti">
+    <li v-for="cliente in clienti" :key="cliente._id">
+                    
+      <div class="riga"><img src="../assets/profilePic.webp" alt="foto profilo" width="100">
+        <div class="colonna"><h2>{{ cliente.nome }} {{ cliente.cognome }}</h2>
+        <button class="rimozione_forzata">Rimuovi profilo</button></div>
+        
+        </div></li>
+  </div>
+
+  <div class="segnalazioni">
+    <li v-for="segnalazione in segnalazioni" :key="segnalazione._id">
+    <div v-if="segnalazione.gestita==false">
+      <div class="riga"><img src="../assets/profilePic.webp" alt="foto profilo" width="100">
+        <div class="colonna"><h2>{{ segnalazione.testo }} {{ segnalazione.segnalato }} {{ segnalazione.data }}</h2>
+        <button class="gestione_segnalazione" @click="gestisci_segnalazione">Gestisci Segnalazione</button></div>
+    </div>
+    </div></li>
+  </div>
+
+
+
+
+</div>
+
+
 </template>
 
 <script>
@@ -41,12 +83,18 @@ export default defineComponent({
     return {
       user: {},
       userRuolo: 1,
+      terapeuti: [],
+      clienti: [],
+      segnalazioni: []
     };
   },
   async mounted() {
 
     
     const token = sessionStorage.getItem("token");
+
+
+
     const options = {
       method: "GET",
       headers: {
@@ -62,7 +110,108 @@ export default defineComponent({
     console.log(JSON.stringify(dati));
     this.user = dati["profile"];
     this.userRuolo = this.user.ruolo;
+
+
+    //catalogo terapeuti
+
+    try {
+      const response = await fetch(
+        `${process.env.VUE_APP_ROOT_API}/catalogo_terapeuti`,
+        {
+          method: "GET",
+          headers: { "Content-Type": "application/json" },
+        }
+      );
+
+      console.log(response["successful"]);
+
+      // if (!response.successful) {
+      //   throw new Error("Unable to get user");
+      // }
+      const informazioni = await response.json();
+      console.log(informazioni);
+      console.log("catalogo: " + JSON.stringify(informazioni["catalogo"]));
+
+      this.terapeuti = informazioni["catalogo"];
+      return this.terapeuti;
+    } catch (error) {
+      console.log(error);
+    }
+
+
+    //catalogo clienti
+
+    try {
+      const response = await fetch(
+        `${process.env.VUE_APP_ROOT_API}/catalogo_clienti`,
+        {
+          method: "GET",
+          headers: { "Content-Type": "application/json" },
+        }
+      );
+
+      console.log(response["successful"]);
+
+      // if (!response.successful) {
+      //   throw new Error("Unable to get user");
+      // }
+      const data = await response.json();
+      console.log(data);
+      console.log("catalogo: " + JSON.stringify(data["catalogo"]));
+
+      this.clienti = data["catalogo"];
+      return this.clienti;
+    } catch (error) {
+      console.log(error);
+    }
+
+
+    //catalogo segnalazioni
+
+    try {
+      const response = await fetch(
+        `${process.env.VUE_APP_ROOT_API}/segnalazione/catalogo_segnalazioni`,
+        {
+          method: "GET",
+          headers: { 
+            "Content-Type": "application/json",
+            "token":token
+          },
+        }
+      );
+
+
+      console.log(response["successful"]);
+
+      // if (!response.successful) {
+      //   throw new Error("Unable to get user");
+      // }
+      const info = await response.json();
+      console.log(info);
+      console.log("catalogo: " + JSON.stringify(info["catalogo"]));
+      this.segnalazioni = info["catalogo"];
+      return this.segnalazioni;
+    } catch (error) {
+      console.log(error);
+    }
   },
+
+  async gestisci_segnalazione(){
+    const token=sessionStorage.getItem("token");
+
+    const options={
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        "x-access-token": token,
+      },
+    };
+    const param = this.$route.params.id
+    const res = await fetch(`${process.env.VUE_APP_ROOT_API}/segnalazione/gestisci/${param}`, options)
+    const i = await res.json()
+    console.log(i.successful)
+    console.log("gestita: "+JSON.stringify(i))
+  }
 });
 </script>
 
